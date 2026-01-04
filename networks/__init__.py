5776d9b3ce8fccf503ecc1e428f1e98d0aaa1190
Jawen <91764573+Jwz599@users.noreply.github.com>
2026-01-04 11:13:31 +0800

    Add files via upload

---
+++ networks/__init__.py
@@ -0,0 +1,13 @@
+#from .lenet import *
+#from .vggnet import *
+#from .resnet import *
+from .wide_resnet import *
+from .wide_resnet_batchensemble import *
+#from .vggnet_ensemble import *
+#from .wide_resnet_ensemble2 import *
+#from .bayesianwide_resnet_ensemble import *
+#from .wide_resnet_ensemble2_div import *
+#from .wide_resnet_ensemble2_latent import *
+
+from .Wide_ResNet_GAMP_SPBNN import *
+
